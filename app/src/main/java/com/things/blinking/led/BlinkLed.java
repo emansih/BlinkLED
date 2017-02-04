3e9eb1623db273ddcbadb89aa3f20dc18a643ed1
Daniel Quah <danielquah148@yahoo.com>
2017-02-05 01:46:25 +0800

    Add license

--- app/src/main/java/com/things/blinking/led/BlinkLed.java
+++ app/src/main/java/com/things/blinking/led/BlinkLed.java
@@ -1,5 +1,21 @@
 package com.things.blinking.led;
 
+/**
+ * Copyright(C) 2017 Daniel Quah
+ * 	
+ * This file is part of com.things.* package
+ *
+ * Blink LED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Blink LED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
